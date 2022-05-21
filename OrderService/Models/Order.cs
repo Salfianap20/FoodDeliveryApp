7ec9860ddedfe9f099b10f9ef5fc7627f90b0d7d
Salfianap20 <salfianap@gmail.com>
2022-05-21 08:29:38 +0700

    troubleshooting order service

--- OrderService/Models/Order.cs
+++ OrderService/Models/Order.cs
@@ -11,7 +11,7 @@ namespace OrderService.Models
         }
 
         public int Id { get; set; }
-        public string Code { get; set; } = null!;
+        public string? Code { get; set; } = null!;
         public int UserId { get; set; }
         public int CourierId { get; set; }
         public string? Longitude { get; set; }
